93290633b9ea73bb4d371587568aa01db46e43f6
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-07-23 18:14:02 +0000

    Live reload broken with '@rollup/plugin-typescript'

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,5 @@
-import {MessageManager} from './MessageManager';
 import WebWorker from 'web-worker:./Worker.ts';
+new WebWorker();
 
-function main(): void {
-    const messageManager = new MessageManager('This instance was created on the main thread');
-
-    const worker = new WebWorker();
-    worker.postMessage({type: 'init', args: 'This instance was created in a worker'});
-
-    messageManager.sayHello('main thread');
-    worker.postMessage({type: 'exec', func:'sayHello', args: 'web worker'});
-
-    messageManager.printMessage();
-    worker.postMessage({type: 'exec', func:'printMessage', args: null});
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-    main();
-});
+console.log('15');
 
